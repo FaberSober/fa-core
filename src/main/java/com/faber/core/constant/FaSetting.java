975a2277beee61aedf771c35935dcd3b125abf97
xu.pengfei <1508075252@qq.com>
2024-10-08 15:22:07 +0800

    base-storage enable multi config

--- src/main/java/com/faber/core/constant/FaSetting.java
+++ src/main/java/com/faber/core/constant/FaSetting.java
@@ -2,6 +2,7 @@ package com.faber.core.constant;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import com.faber.core.enums.ConfigSysStorageActiveEnum;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -64,9 +65,13 @@ public class FaSetting {
     @Data
     public static class File {
         /**
-         * 系统文件存储方式: local-本地存储/qiniu-七牛云/ali-阿里云/tx-腾讯云
+         * 系统文件存储方式: local-本地存储/local-plus-本地存储/qiniu-七牛云/ali-阿里云/tx-腾讯云
          */
         private String saveType;
+        /**
+         * x-file-storage存储的平台
+         */
+        private String savePlatform;
         /**
          * 增加一层最前置路径，可以用于区分不同环境
          */
@@ -89,6 +94,11 @@ public class FaSetting {
 
             String ext = FileUtil.extName(fileName);
             return this.allowFileList.contains(ext);
+        }
+
+        public void updateSaveType(ConfigSysStorageActiveEnum storageActive) {
+            this.saveType = storageActive.getValue();
+            this.savePlatform = storageActive.getDesc();
         }
     }
 
