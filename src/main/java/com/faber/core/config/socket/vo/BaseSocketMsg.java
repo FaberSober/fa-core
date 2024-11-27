e2388ffdfc35945f51bb23bc84cf3bf3f9a81f9b
xupengfei <faberxu@gmail.com>
2024-11-27 21:39:14 +0800

    base-remove tlog

--- src/main/java/com/faber/core/config/socket/vo/BaseSocketMsg.java
+++ src/main/java/com/faber/core/config/socket/vo/BaseSocketMsg.java
@@ -1,7 +1,7 @@
 package com.faber.core.config.socket.vo;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import cn.hutool.json.JSONObject;
+import com.alibaba.fastjson2.JSON;
 import com.faber.core.config.socket.enums.SocketMsgActEnum;
 import lombok.Builder;
 import lombok.Data;
