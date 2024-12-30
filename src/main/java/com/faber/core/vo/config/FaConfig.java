3f0c6f8e2e4cb84cee76d37778dd28693eb2ba17
xu.pengfei <1508075252@qq.com>
2024-12-30 12:20:55 +0800

    base-MenuLayout header add wxminiapp icon

--- src/main/java/com/faber/core/vo/config/FaConfig.java
+++ src/main/java/com/faber/core/vo/config/FaConfig.java
@@ -58,6 +58,10 @@ public class FaConfig implements Serializable {
      */
     private String logoWithText;
     /**
+     * 微信小程序
+     */
+    private String wxminiapp;
+    /**
      * [官网]地址
      */
     private String portalLink;
