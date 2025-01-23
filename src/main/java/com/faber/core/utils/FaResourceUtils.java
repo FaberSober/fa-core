d98371671fddc01e149f4b953fd0d2d7b6262eab
xu.pengfei <1508075252@qq.com>
2025-01-23 10:45:29 +0800

    base-读取sql中文乱码bugfix

--- src/main/java/com/faber/core/utils/FaResourceUtils.java
+++ src/main/java/com/faber/core/utils/FaResourceUtils.java
@@ -4,9 +4,11 @@ import cn.hutool.json.JSONObject;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Spring Resource Helper Class
@@ -19,16 +21,7 @@ public class FaResourceUtils {
     public static String getResourceString(Resource resource) throws IOException {
         //获得文件流，因为在jar文件中，不能直接通过文件资源路径拿到文件，但是可以在jar包中拿到文件流
         InputStream stream = resource.getInputStream();
-        StringBuilder buffer = new StringBuilder();
-        byte[] bytes = new byte[1024];
-        try {
-            for (int n; (n = stream.read(bytes)) != -1; ) {
-                buffer.append(new String(bytes, 0, n));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return buffer.toString();
+        return StreamUtils.copyToString(stream, StandardCharsets.UTF_8);
     }
 
     /**
