1f820a20d6d23a52426a33d98bfce3d1bb0d4001
xu.pengfei <1508075252@qq.com>
2025-02-26 11:01:02 +0800

    core-SqlUtils

--- src/main/java/com/faber/core/utils/SqlUtils.java
+++ src/main/java/com/faber/core/utils/SqlUtils.java
@@ -1,5 +1,11 @@
 package com.faber.core.utils;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * SQL utils
  * @author xu.pengfei
@@ -19,6 +25,24 @@ public class SqlUtils {
         s = s.replaceAll("_", "\\\\_");
         s = s.replaceAll("\\\\", "\\\\\\\\\\\\");
         return s;
+    }
+
+    /**
+     * 执行sql脚本
+     *
+     * @param sql
+     * @throws SQLException
+     */
+    public static void executeSql(Connection conn, String sql) {
+        // 执行sql脚本
+        ScriptRunner runner = new ScriptRunner(conn);
+        runner.setFullLineDelimiter(false);
+        runner.setDelimiter(";");//语句结束符号设置
+        runner.setLogWriter(null);//日志数据输出，这样就不会输出过程
+        runner.setSendFullScript(false);
+        runner.setAutoCommit(true);
+        runner.setStopOnError(true);
+        runner.runScript(new StringReader(sql));
     }
 
 }
