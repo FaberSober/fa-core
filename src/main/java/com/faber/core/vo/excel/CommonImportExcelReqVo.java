742f3619f524bfaf08e75b9b8b04b00a55808d5e
xu.pengfei <1508075252@qq.com>
2025-03-27 16:54:31 +0800

    core-CommonImportExcelReqVo修改为继承LinkedHashMap

--- src/main/java/com/faber/core/vo/excel/CommonImportExcelReqVo.java
+++ src/main/java/com/faber/core/vo/excel/CommonImportExcelReqVo.java
@@ -4,14 +4,22 @@ import lombok.Data;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Data
 @ToString
-public class CommonImportExcelReqVo implements Serializable {
+public class CommonImportExcelReqVo extends LinkedHashMap<String, Object> {
 
     /**
      * 文件ID
      */
     private String fileId;
+
+    public CommonImportExcelReqVo(Map<String, Object> params) {
+        this.putAll(params);
+        this.fileId = params.get("currentPage").toString();
+    }
+
 
 }
