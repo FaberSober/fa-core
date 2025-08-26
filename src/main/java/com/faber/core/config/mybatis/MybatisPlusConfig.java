040f4100d6e801fbcfd204e3ff2e21244005cc9a
xu.pengfei <1508075252@qq.com>
2025-08-26 09:54:08 +0800

    update MyBatisPlusConfig classpath:/mapper/**/*.xml

--- src/main/java/com/faber/core/config/mybatis/MybatisPlusConfig.java
+++ src/main/java/com/faber/core/config/mybatis/MybatisPlusConfig.java
@@ -27,7 +27,6 @@ import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.List;
 
-
 /**
  * Mybatis Plus Config
  *
@@ -45,7 +44,8 @@ public class MybatisPlusConfig {
      * 包含租户ID(tenant_id)字段的表
      * TODO 要支持配置文件
      */
-    private static final List<String> TENANT_EXCLUDE_TABLES = Arrays.asList("tn_tenant", "tn_tenant_user", "tn_tenant_corp", "tn_tenant_corp_agent", "tn_tenant_rbac_menu");
+    private static final List<String> TENANT_EXCLUDE_TABLES = Arrays.asList("tn_tenant", "tn_tenant_user",
+            "tn_tenant_corp", "tn_tenant_corp_agent", "tn_tenant_rbac_menu");
 
     /**
      * 是否是租户表
@@ -53,16 +53,19 @@ public class MybatisPlusConfig {
      * @return
      */
     private boolean isTenantTable(String tableName) {
-        if (TENANT_EXCLUDE_TABLES.contains(tableName)) return false;
+        if (TENANT_EXCLUDE_TABLES.contains(tableName))
+            return false;
         return tableName.startsWith("tn_");
     }
 
     /**
      * 是否是企业表
+     * 
      * @return
      */
     private boolean isCorpTable(String tableName) {
-        if (TENANT_EXCLUDE_TABLES.contains(tableName)) return false;
+        if (TENANT_EXCLUDE_TABLES.contains(tableName))
+            return false;
         return tableName.startsWith("tn_");
     }
 
@@ -72,9 +75,10 @@ public class MybatisPlusConfig {
         /* 数据源 */
         sqlSessionFactory.setDataSource(dataSource);
         /* xml扫描 */
-        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:/mapper/**/*.xml"));
+        sqlSessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/**/*.xml"));
         /* 扫描 typeHandler */
-//        sqlSessionFactory.setTypeHandlersPackage("com.baomidou.mybatisplus.samples.mysql.type");
+        // sqlSessionFactory.setTypeHandlersPackage("com.baomidou.mybatisplus.samples.mysql.type");
         MybatisConfiguration configuration = new MybatisConfiguration();
         configuration.setJdbcTypeForNull(JdbcType.NULL);
         /* 驼峰转下划线 */
@@ -83,58 +87,61 @@ public class MybatisPlusConfig {
 
         // 如果用了分页插件注意先 add TenantLineInnerInterceptor 再 add PaginationInnerInterceptor
         // 多租户
-//        mybatisPlusInterceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
-//            @Override
-//            public String getTenantIdColumn() {
-//                return "tenant_id";
-//            }
-//
-//            @Override
-//            public Expression getTenantId() {
-//                // TO-DO 这里获取上下文的租户ID
-//                return new LongValue(TnTenantContextHandler.getTenantId());
-//            }
-//
-//            // 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
-//            @Override
-//            public boolean ignoreTable(String tableName) {
-//                return !isTenantTable(tableName);
-//            }
-//        }));
+        // mybatisPlusInterceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new
+        // TenantLineHandler() {
+        // @Override
+        // public String getTenantIdColumn() {
+        // return "tenant_id";
+        // }
+        //
+        // @Override
+        // public Expression getTenantId() {
+        // // TO-DO 这里获取上下文的租户ID
+        // return new LongValue(TnTenantContextHandler.getTenantId());
+        // }
+        //
+        // // 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
+        // @Override
+        // public boolean ignoreTable(String tableName) {
+        // return !isTenantTable(tableName);
+        // }
+        // }));
         // 租户下创建的企业
-//        mybatisPlusInterceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
-//            @Override
-//            public String getTenantIdColumn() {
-//                return "corp_id";
-//            }
-//
-//            @Override
-//            public Expression getTenantId() {
-//                // TO-DO 这里获取上下文的租户ID
-//                return new LongValue(TnTenantContextHandler.getCorpId());
-//            }
-//
-//            // 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
-//            @Override
-//            public boolean ignoreTable(String tableName) {
-//                return !isCorpTable(tableName);
-//            }
-//        }));
+        // mybatisPlusInterceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new
+        // TenantLineHandler() {
+        // @Override
+        // public String getTenantIdColumn() {
+        // return "corp_id";
+        // }
+        //
+        // @Override
+        // public Expression getTenantId() {
+        // // TO-DO 这里获取上下文的租户ID
+        // return new LongValue(TnTenantContextHandler.getCorpId());
+        // }
+        //
+        // // 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
+        // @Override
+        // public boolean ignoreTable(String tableName) {
+        // return !isCorpTable(tableName);
+        // }
+        // }));
 
         // 动态表名
-        DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
-        dynamicTableNameInnerInterceptor.setTableNameHandler((sql, tableName) -> {
-            boolean multi = faSetting.getDb() != null && faSetting.getDb().getMultiTables() != null && faSetting.getDb().getMultiTables().contains(tableName.toLowerCase());
-            if (!multi) { // 不是多表名
-                return tableName;
-            }
+        DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor(
+                (sql, tableName) -> {
+                    boolean multi = faSetting.getDb() != null && faSetting.getDb().getMultiTables() != null
+                            && faSetting.getDb().getMultiTables().contains(tableName.toLowerCase());
+                    if (!multi) { // 不是多表名
+                        return tableName;
+                    }
 
-            String suffix = BaseContextHandler.getTableSuffix();
-            if (StrUtil.isEmpty(suffix)) {
-                return tableName;
-            }
-            return tableName + "_" + suffix;
-        });
+                    String suffix = BaseContextHandler.getTableSuffix();
+                    if (StrUtil.isEmpty(suffix)) {
+                        return tableName;
+                    }
+                    return tableName + "_" + suffix;
+                });
         mybatisPlusInterceptor.addInnerInterceptor(dynamicTableNameInnerInterceptor);
 
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
@@ -160,7 +167,8 @@ public class MybatisPlusConfig {
     @Bean
     public GlobalConfig globalConfig() {
         GlobalConfig conf = new GlobalConfig();
-        // conf.setDbConfig(new GlobalConfig.DbConfig().setColumnFormat("`%s`").setPropertyFormat("`%s`"));
+        // conf.setDbConfig(new
+        // GlobalConfig.DbConfig().setColumnFormat("`%s`").setPropertyFormat("`%s`"));
         return conf;
     }
 
