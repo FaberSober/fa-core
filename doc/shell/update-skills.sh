5779dc153e0b45bcdccd52dc6a09ce4031dc4087
xu.pengfei <1508075252@qq.com>
2026-09-17 21:25:15 +0800

    add update skills sh

---
+++ doc/shell/update-skills.sh
@@ -0,0 +1,3 @@
+pnpm dlx skills add https://github.com/openai/codex --skill code-review -g
+pnpm dlx skills add https://github.com/codebeat/agent-skills-codex --skill code-simplifier -g
+pnpm dlx skills add https://github.com/vercel-labs/agent-skills --skill vercel-react-best-practices -g
